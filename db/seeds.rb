b990a77fc8f87058835771169e710b2cef7eecec
Elena Maroto <elena_maroto@hotmail.com>
2017-11-23 17:15:48 +0100

    cocktails working

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Deleting all tables contents"
+
+Dose.destroy_all
+Cocktail.destroy_all
+Ingredient.destroy_all
+
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "sugar")
+Ingredient.create(name: "Clove")
+Ingredient.create(name: "grenade")
+Ingredient.create(name: "rhum")
+
+puts "Ingredients created!"
